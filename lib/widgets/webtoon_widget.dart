700924c74aa62e7d9d201064666c783fe9118b00
minwoo129 <key0129mw@naver.com>
2024-03-12 10:29:14 +0900

    강의 실습 코드 작성
    - ListView item 위젯 분리

---
+++ lib/widgets/webtoon_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix_flutter_ex/models/webtoon_model.dart';
+
+class Webtoon extends StatelessWidget {
+  final WebtoonModel webtoon;
+  const Webtoon({
+    super.key,
+    required this.webtoon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 250,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3))
+              ]),
+          child: Image.network(
+            webtoon.thumb,
+            headers: const {
+              "User-Agent":
+                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          webtoon.title,
+          style: const TextStyle(
+            fontSize: 22,
+          ),
+        ),
+      ],
+    );
+  }
+}
